7a2704dfa6620f75cd4e83172c722070a94ecb07
plbthien <plbthien01062004@gmail.com>
2025-08-08 12:37:36 +0700

    i18n(project_todo): Add Vietnamese translation file

---
+++ gopify_project_todo/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Vietnamese Translation for Project Todo',
+    'version': '1.0',
+    'category': 'Localization',
+    'summary': 'Vietnamese translation for Project Todo module',
+    'depends': ['base', 'project_todo'],
+    'data': [],
+    'aplications': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
